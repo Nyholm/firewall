807e472962f3bd239370b8eb91d7153928e29eb3
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-06-27 21:49:57 +0200

    Make sure you can add rules

--- src/Firewall/Firewall.php
+++ src/Firewall/Firewall.php
@@ -6,6 +6,9 @@ use Psr\Http\Message\ServerRequestInterface;
 
 class Firewall
 {
+    /**
+     * @var array|Rule[]
+     */
     private $rules;
 
     /**
@@ -25,5 +28,27 @@ class Firewall
                 return $rule->apply($request);
             }
         }
+    }
+
+    /**
+     * Insert a rule to the rule chain. Negative value or zero will insert the rule first.
+     *
+     * @param Rule $rule
+     * @param $index
+     */
+    public function addRule(Rule $rule, $index = 0)
+    {
+        if ($index <= 0) {
+            // Add as first rule
+            array_unshift($this->rules, $rule);
+        }
+
+        if ($index >= count($this->rules)) {
+            // Add as last rule
+            array_push($this->rules, $rule);
+        }
+
+        // Insert in the middle or the array
+        $this->rules = array_splice($this->rules, $index, 0, $rule);
     }
 }
